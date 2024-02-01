1312f5c832f8e0679c49318b0e8ee565d2802a92
Felipe Oriani <felipeoriani@gmail.com>
2024-02-01 20:15:25 -0300

    Add flight repository for DynamoDb

---
+++ src/core/infrastructure/repositories/flightRepository.ts
@@ -0,0 +1,18 @@
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
+import { ddb } from '../dynamo.js'
+import { BaseDynamoRepository } from './baseDynamoRepository.js'
+import { IFlightRepository, Flight, flightPk, flightSkPrefix } from '../../domain/model/flight.js'
+
+/**
+ * Implementation for IFlightRepository using DynamoDB as the data source.
+ * @param ddbDocumentClient Instance for DynamoDBDocumentClient.
+ * @returns An instance of the derived repository class to handle Task entity.
+ * @see BaseDynamoRepository
+ * @see IFlightRepository
+ * @see Flight
+ */
+export class FlightRepository extends BaseDynamoRepository<Flight> implements IFlightRepository {
+  constructor(ddbDocumentClient: DynamoDBDocumentClient = ddb) {
+    super(ddbDocumentClient, flightPk, flightSkPrefix)
+  }
+}
