b1c07ffb1287a1a6625ab2cbdd4a0d025c770b3f
Felipe Oriani <felipeoriani@gmail.com>
2024-02-02 13:31:42 -0300

    Add flight service

---
+++ src/core/application/flightService.ts
@@ -0,0 +1,31 @@
+import { Flight, IFlightRepository, IFlightService } from '../domain/model/flight.js'
+import { GetPagedResult } from '../domain/repository/repository.js'
+import { FlightRepository } from '../infrastructure/repositories/flightRepository.js'
+
+export class FlightService implements IFlightService {
+  constructor(private flightRepository: IFlightRepository = new FlightRepository()) {}
+
+  public async get(id: string): Promise<Flight | undefined> {
+    const flight = await this.flightRepository.get(id)
+    if (!flight) {
+      return undefined
+    }
+    return flight
+  }
+
+  public getPaged(count?: number | undefined, nextToken?: string | undefined): Promise<GetPagedResult<Flight>> {
+    return this.flightRepository.getPaged(count, nextToken)
+  }
+
+  public create(model: Flight): Promise<Flight> {
+    return this.flightRepository.create(model)
+  }
+
+  public update(id: string, model: Flight): Promise<Flight> {
+    return this.flightRepository.update(id, model)
+  }
+
+  public delete(id: string): Promise<boolean> {
+    return this.flightRepository.delete(id)
+  }
+}
