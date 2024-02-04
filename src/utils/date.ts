f138d7c62683d8f6a30125586c4bd7b83c54b0a6
Felipe Oriani <felipeoriani@gmail.com>
2024-02-04 14:11:56 -0300

    Add date utils functions

---
+++ src/utils/date.ts
@@ -0,0 +1,31 @@
+/**
+ * Get unix time format number from a date.
+ * @param date Date to be converted into unix time.
+ * @returns Unix time format number.
+ */
+export function toUnixTime(date: Date): number {
+  return Math.floor(date.getTime() / 1000)
+}
+
+/**
+ * Get the date from a unix time format number.
+ * @param epoch Unix time format number.
+ * @returns Date instance represented by epoch number.
+ */
+export function fromUnixTime(epoch: number): Date {
+  return new Date(epoch * 1000)
+}
+
+/**
+ * Format the date with dashes in the pattern: dd-Mon-yyyy.
+ * @param date Date to be formatted.
+ * @returns Formatted date string.
+ */
+export function formatDate(date: Date): string {
+  const monthsAbbr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  const day = date.getDate()
+  const monthIndex = date.getMonth()
+  const year = date.getFullYear()
+  const formattedDate = `${day}-${monthsAbbr[monthIndex]}-${year}`
+  return formattedDate
+}
