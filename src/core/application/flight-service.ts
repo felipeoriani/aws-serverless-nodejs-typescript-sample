194ad7a37f276cd2d5fddceaf16672efc1ce205d
Felipe Oriani <felipeoriani@gmail.com>
2024-02-06 23:53:15 -0300

    Change flight repository and service to handle Flight type

--- src/core/application/flight-service.ts
+++ src/core/application/flight-service.ts
@@ -1,61 +1,52 @@
 import { ValidateableResponse } from 'src/utils/validateable-response.js'
-import { Flight, FlightModel, IFlightRepository, IFlightService, flightValidator } from '../domain/model/flight.js'
+import { Flight, IFlightRepository, IFlightService, flightValidator } from '../domain/model/flight.js'
 import { GetPagedResult } from '../domain/repository/base-repository.js'
 import { FlightRepository } from '../infrastructure/repositories/flight-repository.js'
 
 export class FlightService implements IFlightService {
   constructor(private flightRepository: IFlightRepository = new FlightRepository()) {}
 
-  private map(flight: Flight): FlightModel {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { pk, gsi1pk, gsi1sk, ...model } = flight
-    return model
-  }
-
-  public async get(id: string): Promise<ValidateableResponse<FlightModel>> {
-    const flight = await this.flightRepository.get(id)
-    if (!flight) {
+  public async get(id: string): Promise<ValidateableResponse<Flight>> {
+    const model = await this.flightRepository.get(id)
+    if (!model) {
       return {
         errors: [`There is not flight for the id '${id}'`],
       }
     }
-    const model = this.map(flight)
     return { model }
   }
 
   public async getPaged(
     count?: number | undefined,
     nextToken?: string | undefined
-  ): Promise<ValidateableResponse<GetPagedResult<FlightModel>>> {
+  ): Promise<ValidateableResponse<GetPagedResult<Flight>>> {
     const pagedResult = await this.flightRepository.getPaged(count, nextToken)
-    const items = pagedResult.items.map(this.map)
     return {
       model: {
         count: pagedResult.count,
         nextToken: pagedResult.nextToken,
-        items,
+        items: pagedResult.items,
       },
     }
   }
 
-  public async create(input: FlightModel): Promise<ValidateableResponse<FlightModel>> {
+  public async create(input: Flight): Promise<ValidateableResponse<Flight>> {
     const validation = flightValidator.validate(input)
     if (validation.error) {
       const errors = validation.error.details.map((x) => x.message)
       return { errors }
     }
-    const entity = await this.flightRepository.create(input as Flight)
-    const model = this.map(entity)
+    const model = await this.flightRepository.create(input)
     return { model }
   }
 
-  public async update(id: string, input: FlightModel): Promise<ValidateableResponse<FlightModel>> {
+  public async update(id: string, input: Flight): Promise<ValidateableResponse<Flight>> {
     const validation = flightValidator.validate(input)
     if (validation.error) {
       const errors = validation.error.details.map((x) => x.message)
       return { errors }
     }
-    const model = await this.flightRepository.update(id, input as Flight)
+    const model = await this.flightRepository.update(id, input)
     return { model }
   }
 
--- src/core/infrastructure/repositories/flight-repository.ts
+++ src/core/infrastructure/repositories/flight-repository.ts
@@ -2,6 +2,7 @@ import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
 import { ddb } from '../dynamo.js'
 import { BaseDynamoRepository } from './base-dynamodb-repository.js'
 import { IFlightRepository, Flight } from '../../domain/model/flight.js'
+import { fromISOToDate } from '../../../utils/date.js'
 
 /**
  * Implementation for IFlightRepository using DynamoDB as the data source.
@@ -11,8 +12,23 @@ import { IFlightRepository, Flight } from '../../domain/model/flight.js'
  * @see IFlightRepository
  * @see Flight
  */
+
 export class FlightRepository extends BaseDynamoRepository<Flight> implements IFlightRepository {
   constructor(ddbDocumentClient: DynamoDBDocumentClient = ddb) {
     super(ddbDocumentClient, 'flight')
+  }
+
+  protected override mapEntity(record: Record<string, unknown>): Flight {
+    record.date = fromISOToDate(record.date as string)
+    return record as Flight
+  }
+
+  protected override handleModel(entity: Flight): Flight {
+    const model = entity as Record<string, unknown>
+    const date = entity.date.toISOString()
+    model.date = date
+    model.gsi1pk = `${entity.from}#${entity.to}`
+    model.gsi1sk = date
+    return entity as Flight
   }
 }
