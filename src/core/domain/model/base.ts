fd07530338b162e84bdf3e0a3b8f5da2101b2105
Felipe Oriani <felipeoriani@gmail.com>
2024-02-06 23:51:52 -0300

    Change flight models

--- src/core/domain/model/base.ts
+++ src/core/domain/model/base.ts
@@ -1,16 +1,5 @@
+/** Base Entity type to provide the base funcionality to all the entities of the project */
 export type BaseEntity = {
   /** Sort Key of the item. Generally used as the identifier of the object. */
   id: string
-}
-
-/**
- * Base model for all entities.
- */
-export type Entity = BaseEntity & {
-  /** Partition Key of the item. It can be use to seggregate objects by Partition Key. */
-  pk: string
-  /** Partition Key for Global Secondary Index 1. */
-  gsi1pk?: string
-  /** Sort Key for Global Secondary Index 1 */
-  gsi1sk?: string
 }
--- src/core/domain/model/flight.ts
+++ src/core/domain/model/flight.ts
@@ -1,33 +1,34 @@
 import { ValidateableResponse } from 'src/utils/validateable-response.js'
 import { GetPagedResult, IBaseRepository } from '../repository/base-repository.js'
-import { Entity } from './base.js'
+import { BaseEntity } from './base.js'
 import joi from 'joi'
 
 /** FLight entity that represents a flight containing all the info */
-export type Flight = Entity & {
+export type Flight = BaseEntity & {
+  code: string
   from: string
   to: string
   date: Date
+  airline: string
 }
-
-/** Flight Model to responde from the UseCases services for the application */
-export type FlightModel = Omit<Flight, Exclude<keyof Entity, 'id'>>
 
 export interface IFlightRepository extends IBaseRepository<Flight> {}
 
 export interface IFlightService {
-  get(id: string): Promise<ValidateableResponse<FlightModel>>
+  get(id: string): Promise<ValidateableResponse<Flight>>
   getPaged(
     count?: number | undefined,
     nextToken?: string | undefined
-  ): Promise<ValidateableResponse<GetPagedResult<FlightModel>>>
-  create(input: FlightModel): Promise<ValidateableResponse<FlightModel>>
-  update(id: string, input: FlightModel): Promise<ValidateableResponse<FlightModel>>
+  ): Promise<ValidateableResponse<GetPagedResult<Flight>>>
+  create(input: Flight): Promise<ValidateableResponse<Flight>>
+  update(id: string, input: Flight): Promise<ValidateableResponse<Flight>>
   delete(id: string): Promise<ValidateableResponse<boolean>>
 }
 
 export const flightValidator = joi.object<Flight>().keys({
+  code: joi.string().required().alphanum().min(3).max(12),
   from: joi.string().required().min(3).max(100),
   to: joi.string().required().min(3).max(100),
   date: joi.date().required(),
+  airline: joi.string().required().max(50),
 })
