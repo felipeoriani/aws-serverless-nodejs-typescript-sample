2a898d6fd9ea2f197ed9485d31936298a176a3ad
Felipe Oriani <felipeoriani@gmail.com>
2024-03-23 19:20:32 -0300

    Validate query parameter in get /flights route

--- src/functions/get-flight.ts
+++ src/functions/get-flight.ts
@@ -1,10 +1,41 @@
-import { APIGatewayProxyResult } from 'aws-lambda'
-import { ok } from '../utils/response.js'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { badRequest, ok } from '../utils/response.js'
 import { FlightService } from '../core/application/flight-service.js'
-import { IFlightService } from 'src/core/domain/model/flight.js'
+import { IFlightService } from '../core/domain/model/flight.js'
+import { isDate } from '../utils/date.js'
 
-export const handler = async (): Promise<APIGatewayProxyResult> => {
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  if (!event.queryStringParameters) {
+    return badRequest('Filter must be provided to request the flights.')
+  }
+
+  const { startDate, endDate, from, to, nextToken } = event.queryStringParameters
+
+  if (!startDate) {
+    return badRequest('The startDate query parameter is required.')
+  }
+
+  if (isDate(startDate)) {
+    return badRequest('The startDate query parameter must be a valid date.')
+  }
+
+  if (!endDate) {
+    return badRequest('The endDate query parameter is required.')
+  }
+
+  if (isDate(endDate)) {
+    return badRequest('The endDate query parameter must be a valid date.')
+  }
+
+  if (!from) {
+    return badRequest('The from airport query parameter is required.')
+  }
+
+  if (!to) {
+    return badRequest('The to airport query parameter is required.')
+  }
+
   const flightService: IFlightService = new FlightService()
-  const result = await flightService.getPaged()
+  const result = await flightService.getPaged(startDate, endDate, from, to, 10, nextToken)
   return ok(result.model)
 }
