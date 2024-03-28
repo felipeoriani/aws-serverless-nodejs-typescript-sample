30aa21a6331f8354875c90b822d1a529701b623b
Felipe Oriani <felipeoriani@gmail.com>
2024-03-28 16:03:08 -0300

    Add implementation for notification service to start check-in process

---
+++ src/core/application/notification-service.ts
@@ -0,0 +1,25 @@
+import { IFlightRepository } from '../domain/model/flight.js'
+import { MessageQueue } from '../domain/utils/message-queue.js'
+import { FlightRepository } from '../infrastructure/repositories/flight-repository.js'
+import { SqsMessage } from '../infrastructure/utils/sqs-message.js'
+
+export class NotificationService {
+  constructor(
+    private readonly flightRepository: IFlightRepository = new FlightRepository(),
+    private readonly messageQueue: MessageQueue = new SqsMessage()
+  ) {}
+
+  async notify(startDate: Date): Promise<number> {
+    // get all the flights ready to be checkin
+    const flights = await this.flightRepository.getFlightsToCheckIn(startDate)
+
+    // prepare all the messages to be sent
+    const messages = flights.map((flight) => this.messageQueue.sendCheckInFlightMessage({ flightId: flight.id }))
+
+    // push all the messages in parallel to the SQS instance
+    const results = await Promise.all(messages)
+
+    // return the number of requests that succeed
+    return results.filter((x) => x).length
+  }
+}
--- src/functions/check-in-start.ts
+++ src/functions/check-in-start.ts
@@ -1,6 +1,12 @@
-export const handler = async (event: unknown) => {
-  // get all the flights ready to be checkin
-  // for each flight, push a message into sqs with { flightId }
+import { NotificationService } from 'src/core/application/notification-service.js'
 
+export const handler = async (event: unknown) => {
   console.log(`Check-in event: ${JSON.stringify(event)}`)
+
+  const currentDate = new Date()
+  const notificationService = new NotificationService()
+
+  const result = await notificationService.notify(currentDate)
+
+  console.log(`A total of ${result} message(s) where sent to start check-in process on the flight.`)
 }
